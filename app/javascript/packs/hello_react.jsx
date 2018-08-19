4a73b0776404acbc6c763bd247221ce7c16c0b12
webelf000 <m63974529@gmail.com>
2018-08-19 19:45:12 +0300

    Update hello_reach component

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -1,26 +1,31 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-
+import {Page, Card, Button, Thumbnail} from '@shopify/polaris';
 const Hello = props => (
-  <div>Hello {props.name}!</div>
+   <Page title="Products">
+    {props.products.map((product, index) => (
+    <Card key={index}
+      title={product.title}
+      primaryFooterAction={{
+        content: 'View',
+        url: 'https://${shop_session.url}/admin/products/${product.id}',
+      }}
+      sectioned
+    >
+        <Thumbnail
+          source={product.images[0].src}
+          alt={product.title}
+          size="large"
+        />
+
+    </Card>
+    ))}
+  </Page>
 )
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
+// Render component with data
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
+  const node = document.getElementById('hello-react')
+  const data = JSON.parse(node.getAttribute('data'))
+ReactDOM.render(<Hello {...data} />, node)
 })
